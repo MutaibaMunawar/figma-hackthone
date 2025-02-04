c69afc70b16cfea46daa42d6b54c4c65e5ea2dfe
MutaibaMunawar <mutaibachohan8@gmail.com>
2025-02-04 08:53:05 +0500

    main

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,32 @@
+// export const apiVersion =
+//   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-22'
+
+// export const dataset = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+// )
+
+// export const projectId = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+// )
+
+// export const token = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_TOKEN,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_TOKEN'
+// )
+
+
+// function assertValue<T>(v: T | undefined, errorMessage: string): T {
+//   if (v === undefined) {
+//     console.error(`Error: ${errorMessage}`);  // For debugging
+//     throw new Error(errorMessage)
+//    }
+ 
+//    return v
+// }
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-22'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-07'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
@@ -11,17 +38,10 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
-export const token = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_TOKEN,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_TOKEN'
-)
-
-
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    console.error(`Error: ${errorMessage}`);  // For debugging
     throw new Error(errorMessage)
-   }
- 
-   return v
+  }
+
+  return v
 }
